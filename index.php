59d0342ddaab1aeb61e60d848f22314132098a8b
Ксения Стрижак <55100009+Swift-13@users.noreply.github.com>
2020-11-02 18:59:31 +0300

    Add files via upload
    completed task for lesson 1

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+	//$name = "Swift";
+	//echo "Hello ".$name."<br>";
+	//define(LASTNAME, Jonathan);
+	//echo LASTNAME;
+
+	/*$precise1 = 1.5;
+	$precise2 = 1.5e4;
+	$precise3 = 6E-8;
+	echo "$precise1 | $precise2 | $precise3";
+
+	$a = 1;
+	echo "$a";
+	echo '$a';*/
+	
+	$a = 'Hello,';
+	$b = 'world';
+	$c = $a . $b;
+	echo $c;
+
+?>
